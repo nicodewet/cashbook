a452a8c49f6ffa7d85125894d59f94e006a530f0
Nico de Wet <nico@nicodewet.com>
2018-08-07 20:14:06 +1200

    adding BusinessTransaction model which is the heart of the cashbook

---
+++ src/main/kotlin/core/entity/BusinessTransaction.kt
@@ -0,0 +1,74 @@
+package com.thorgil.cashbook.core.entity
+
+import java.time.LocalDate
+import java.time.LocalDateTime
+
+/**
+ * BusinessTransactions are a record of all payments (expenses) and receipts (income).
+ *
+ * To be clear a BusinessTransaction always has a movement of money associated with it (and so can be reconciled with
+ * one of the Company's bank accounts) with the exception of BusinessTransactions that have a scheduledDate set and no
+ * completedDate. The combination of the former scenario can be used to model future expected BusinessTransactions (this
+ * is to help with recording liabilities such as various forms of tax to the IRD).
+ *
+ * In terms of modelling future transactions, when it comes to certain types of liabilities (e.g. paying over employee
+ * income tax to the IRD) it is likely that one future transaction will be linked to many immediately completed
+ * BusinessTransactions. This is because one payment to the IRD (in future) would be linked to many individual
+ * parentTransactions.
+ *
+ * Future BusinessTransactions could also be used to model when the business is expecting to have invoices paid.
+ *
+ * In terms of recording a BusinessTransactionType initially what matters is whether it is of type EXPENSE (debit) or
+ * INCOME (credit). Breaking it down further can be done of course.
+ *
+ * @param createdTimestamp
+ * @param lastUpdateTimestamp
+ * @param scheduledDate if this is set and completed is not set then this is a future (scheduled) transaction
+ * @param completedDate if this is set the transaction occurred (reconciliation can occur) and no further action
+ *                      is expected
+ * @param type
+ * @param amountInCents
+ * @param parentTransactions
+ * @param evidenceLink
+ */
+class BusinessTransaction(val createdTimestamp: LocalDateTime,
+                          val lastUpdateTimestamp: LocalDateTime,
+                          val scheduledDate: LocalDate?,
+                          val completedDate: LocalDate?,
+                          val type: BusinessTransactionType,
+                          val amountInCents: Int,
+                          val parentTransactions: List<BusinessTransaction>,
+                          val evidenceLink: String?) {
+}
+
+enum class BusinessTransactionType {
+    EXPENSE {
+        override fun parent() = null
+    },
+    INCOME {
+        override fun parent() = null
+    },
+    BANK_FEE_PAYMENT {
+        override fun parent() = EXPENSE
+    },
+    SALARY_PAYMENT {
+        override fun parent() = EXPENSE
+    },
+    SUPPLIER_INVOICE_PAYMENT {
+        override fun parent() = EXPENSE
+    },
+    IRD_SALARY_OVERHEAD_PAYMENT {
+        override fun parent() = EXPENSE
+    },
+    IRD_GST_PAYMENT {
+        override fun parent() = EXPENSE
+    },
+    INVOICE_PAYMENT {
+        override fun parent() = INCOME
+    },
+    OWNER_CONTRIBUTION_PAYMENT {
+        override fun parent() = INCOME
+    };
+
+    abstract fun parent(): BusinessTransactionType?
+}
